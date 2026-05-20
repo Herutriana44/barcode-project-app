90dc2be9d576fc2c442c6536e9ca45dca0c0bf22
Heru Triana <herutriana44@gmail.com>
2026-05-20 12:57:27 +0700

    [GEMINI_TERMUX] Update label isi quantity to qty_sub_pack

--- resources/views/item-barcodes/partials/qc-label-isi-card.blade.php
+++ resources/views/item-barcodes/partials/qc-label-isi-card.blade.php
@@ -10,7 +10,7 @@
     $beratPcsGram = $item->berat_per_pcs_gram !== null ? (float) $item->berat_per_pcs_gram : 0.0;
     $beratStr = number_format($beratPcsGram, 2, '.', '');
 
-    $qtyInPack = (int) ($qtyInPack ?? 1);
+    $qtyInPack = (int) ($item->qty_sub_pack ?? 0);
     $qtyStr = $qtyInPack > 0 ? (string) (int) $qtyInPack : '';
 
         <div class="isi-barcode-box">
