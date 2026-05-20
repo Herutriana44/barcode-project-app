2b7867ec7b68fd3d9269fe41a7e676c3df748f81
Heru Triana <herutriana44@gmail.com>
2026-05-20 12:58:58 +0700

    [GEMINI_TERMUX] Restore missing template markup in label isi

--- resources/views/item-barcodes/partials/qc-label-isi-card.blade.php
+++ resources/views/item-barcodes/partials/qc-label-isi-card.blade.php
@@ -12,6 +12,25 @@
 
     $qtyInPack = (int) ($item->qty_sub_pack ?? 0);
     $qtyStr = $qtyInPack > 0 ? (string) (int) $qtyInPack : '';
+@endphp
+
+<article class="isi-card">
+    <div class="isi-top">
+        <div class="isi-no">{{ $itemBarcode->id }}</div>
+        <div class="isi-company">PT. TASM</div>
+    </div>
+
+    <div class="isi-body">
+        <div class="isi-fields-top">
+            <div class="row"><span class="k">Part No</span><span class="sep">:</span><span class="v">{{ $partNo }}</span></div>
+            <div class="row"><span class="k">Part Name</span><span class="sep">:</span><span class="v">{{ $partName }}</span></div>
+        </div>
+        <div class="isi-fields">
+            <div class="row"><span class="k">QTY</span><span class="sep">:</span><span class="v">{{ $qtyStr }}{{ $qtyStr !== '' ? ' Pcs' : '' }}</span></div>
+            <div class="row"><span class="k">Berat</span><span class="sep">:</span><span class="v">{{ $beratStr }}{{ $beratStr !== '' ? ' gram' : '' }}</span></div>
+            <div class="row"><span class="k">Tgl prod</span><span class="sep">:</span><span class="v">{{ $prod }}</span></div>
+            <div class="row"><span class="k">Checker</span><span class="sep">:</span><span class="v">{{ $checker }}</span></div>
+        </div>
 
         <div class="isi-barcode-box">
             <div class="isi-barcode-wrap">
@@ -20,4 +39,3 @@
         </div>
     </div>
 </article>
-
